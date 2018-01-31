77ee09962448f0af81e713c028dd2a6e6a0c36dc
Eric Ruggieri <eric.ruggieri@turner.com>
2018-01-31 14:31:48 -0500

    Updating to the newest version of the server with username and password as inputs

--- serverApp.js
+++ serverApp.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+
+
 const app = express();
+
+// function supportCrossOriginScript(req, res, next) {
+//     res.status(200);
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//
+//     // res.header("Access-Control-Allow-Headers", "Origin");
+//     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     // res.header("Access-Control-Allow-Methods","POST, OPTIONS");
+//     // res.header("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE, PUT, HEAD");
+//     // res.header("Access-Control-Max-Age","1728000");
+//     next();
+// }
+// app.all('/result', supportCrossOriginScript);
 const mongoose = require('mongoose');
-var url = "mongodb://@ericproductionhome-shard-00-00-qxfv2.mongodb.net:27017,ericproductionhome-shard-00-01-qxfv2.mongodb.net:27017,ericproductionhome-shard-00-02-qxfv2.mongodb.net:27017/Wedding?ssl=true&replicaSet=EricProductionHome-shard-0&authSource=admin";
+var userName = "";
+var password = "";
+process.argv.forEach(function (val, index) {
+  if(index==2) {
+    userName=val;
+  }
+  else if (index==3) {
+    password=val;
+  }
+});
+
+var url = "mongodb://"+userName+":"+password+"@ericproductionhome-shard-00-00-qxfv2.mongodb.net:27017,ericproductionhome-shard-00-01-qxfv2.mongodb.net:27017,ericproductionhome-shard-00-02-qxfv2.mongodb.net:27017/Wedding?ssl=true&replicaSet=EricProductionHome-shard-0&authSource=admin";
 mongoose.connect(url);
 var db = mongoose.connection;
 
 const Guests = require('./app/guestsModel.js');
 
+app.enable("jsonp callback");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 db.once('open', function() {
@@ -19,25 +48,28 @@ db.once('open', function() {
 app.get('/guests', (req,res) => {
   Guests.find((err,guests) => {
     if(err)
-      console.log(handleError(err));
-    res.json(guests);
+      console.log(err);
+    res.jsonp(guests);
   });
 });
 
 app.post('/guests', (req,res) => {
+  console.log("Eric");
   Guests.create({
     name: req.query.name,
     family: req.query.family,
-    bridal_party: req.query.bridal_party,
-    notDrinking: false,
-    plusOne: false
+    bridal_party: false,
+    notDrinking: req.query.notDrinking,
+    plusOne: req.query.plusOne
   }, (err,guests) => {
     if(err)
       console.log(handleError(err));
+
     Guests.find((err,guests) => {
       if(err)
         console.log(handleError(err));
-      res.json(guests);
+      // res.setHeader('Content-Type', 'application/javascript');
+      // res.jsonp(guests);
     });
   });
 });
